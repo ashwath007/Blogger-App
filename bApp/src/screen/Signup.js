7cb64ac72571ff5fd76dcc7cc8236eeab0388889
vicky <srivicky2000@gmail.com>
2021-03-20 11:14:23 +0530

    SignUp

--- bApp/src/screen/Signup.js
+++ bApp/src/screen/Signup.js
@@ -1,9 +1,42 @@
-import React from 'react'
-import {View,StyleSheet,Text} from 'react-native';
+import React,{useState} from 'react'
+import {View,StyleSheet,ScrollView,TouchableOpacity} from 'react-native';
+
+import {Container,
+Form,
+Item,
+Input,
+Text,
+Button,
+Thumbnail,
+Content} from 'native-base'
+
+
+import storage from '@react-native-firebase/storage'
+import ProgessBar from 'react-native-progress'
+
+
+import ImagePicker from 'react-native-image-picker'
+import {options} from '../utils/options'
+
+
+import propTypes from 'prop-types'
+import {signUp} from '../action/auth'
+import {connect} from 'react'
+
+
+const SignUp = ({signUp}) => {
+
+    const [name,setName] = useState('')
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [instaUserName,setInstaUserName] = useState('')
+    const [country,setCountry] = useState('')
+    const [bio,setBio] = useState('')
+    const [image,setImage] = useState('https://firebase.google.com/downloads/brand-guidelines/PNG/logo-vertical.png')
 
 
 
-const SignUp = () => {
+
     return(
         <View>
             <Text>
@@ -13,4 +46,12 @@ const SignUp = () => {
     )
 }
 
-export default SignUp
+const mapDispatchToProps = {
+    signUp: (data) => signUp(data)
+}
+
+SignUp.propTypes = {
+    signUp: propTypes.func.isRequired
+}
+
+export default connect(null,mapDispatchToProps)(SignUp)
