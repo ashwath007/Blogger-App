7a087fa5da2c97ffe87eed11f53aa23c82290e49
vicky <srivicky2000@gmail.com>
2021-03-26 23:53:58 +0530

    Working on Reading Part

--- bApp/src/screen/Read.js
+++ bApp/src/screen/Read.js
@@ -1,16 +1,20 @@
 import React,{useEffect,useState} from 'react'
-import {View,Text} from 'react-native'
+import {View,Text,Image,SafeAreaView,ScrollView} from 'react-native'
 import database from '@react-native-firebase/database';
-
 // import {getPostById} from '../action/post'
 import EmptyContainer from '../components/EmptyContainer'
-
+import {Right,Left,H3} from 'native-base'
+import Tts from 'react-native-tts';
+import { material,sanFranciscoSpacing,robotoWeights   } from 'react-native-typography'
 const Read = ({route}) => {
     
     const [location, setLocation] = useState('')
     const [description, setDescription] = useState('')
+    const [image, setImage] = useState('')
+    const [author, setAuthor] = useState('')
+
     const [story, setStory] = useState('')
-    const [loading,setLoading] = useState(false)
+    const [loading,setLoading] = useState(true)
 
         const getPostById = (ID) => {
             setLoading(true)
@@ -20,10 +24,12 @@ const Read = ({route}) => {
             .on('value', (snapshot) => {
                 console.log('USER Data: ', snapshot.val())
                 if (snapshot.val()) {
-                    console.log(snapshot.val())
+                    console.log(snapshot.val().picture)
                     setLocation(snapshot.val().location)
                     setDescription(snapshot.val().description)
                     setStory(snapshot.val().story)
+                    setImage(snapshot.val().picture)
+                    setAuthor(snapshot.val().by)
                     setLoading(false)
                 } else {
                     console.log("Error")
@@ -45,14 +51,49 @@ const Read = ({route}) => {
       return <EmptyContainer/>
    }
    else{
-   return (<>
-       <Text>
-           {location}
-       </Text>
-       <Text>
+   return (
+      <ScrollView contentContainerStyle={{flexGrow: 1}} style={{backgroundColor:'#fff'}}>
+    {/* <View
+            style={{ height: '50%', width: '100%',position: 'relative',backgroundColor:'black'}}
+    
+    >
+    {console.log("Image",image)}
+
+    <ImageBackground  
+            source={{uri:image}}
+            style={{height: '100%', width: '100%', flex: 1,opacity: 0.5}}
+          />
+    </View>  */}
+    {/* <Image
+        source={{uri:image}}
+        style={{height:'30%',width:'100%'}}
+    />
+   */}
+       {/* <Text style={{color:'#E21717', fontWeight:'bold'}}>
+           {author}
+       </Text> */}
+       <View style={{padding:15,borderRadius:12}}>
+
+        <View style={{padding:20,backgroundColor:'#EF5354',borderRadius:12}}>
+        <Text
+            style={{fontSize:8,fontSize: 20,color:'white',
+                letterSpacing: sanFranciscoSpacing(34)}}
+        >
            {description}
        </Text>
-       </>)
+     
+        </View>
+        </View>
+
+     <View style={{padding:20}}>
+
+     <Text style={material.title}>
+           {story}
+       </Text>
+     </View>
+       </ScrollView>
+
+      )
    }
         
     
