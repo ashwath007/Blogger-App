0f2f0f46032f63b3e760160b86680b9f71becc47
vicky <srivicky2000@gmail.com>
2021-04-06 10:57:46 +0530

    Working on It

--- bApp/src/screen/Editor.js
+++ bApp/src/screen/Editor.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {SafeAreaView,ScrollView,StyleSheet,Image,TouchableOpacity,Alert } from 'react-native'
+import { Searchbar } from 'react-native-paper';
+import { View,Container, Header, Content, Card, CardItem, Thumbnail,Item,Input, Text, Button, Icon, Left, Body, Right } from 'native-base';
 
-import {View,Text} from 'react-native';
 
 
 const Editor = () => {
+    const [pass,setPass] = useState("")
+    
+    const requestPasscode = () => {
+        console.log("pass",pass);
+        if(!pass){
+
+            Alert.alert(
+                "Please enter the correct code")
+        
+        }
+        else if(pass == "start"){
+                //Have navigation
+        }
+        // TODO: Need to work on post Request 
+    }
 
     return(
+        <SafeAreaView>
         <View>
-            <Text>
-            Editor
-            </Text>
-        </View>
+                <View style={{backgroundColor:'#fff',padding:20,justifyContent:'center',marginTop:90}}> 
+                <Text style={{color:'#242B2E',fontSize:23,marginTop:23,alignItems:'center',marginBottom:26}}>
+                    Enter the passcode to enter Editor Dashboard
+                </Text>
+         
+          <Item>
+            <Input placeholder='Regular Textbox' value={pass} onChangeText={(text) => setPass(text)} />
+          </Item>
+      
+        <TouchableOpacity onPress={() => requestPasscode()} style={{backgroundColor:'#242B2E',borderRadius:8,padding:10,marginTop:23,alignItems:'center'}}>
+          <Text style={{color:'white'}}>
+              Request
+          </Text>
+      </TouchableOpacity>
+    
+     
+                </View>
+            </View>
+            </SafeAreaView>
     )   
 }
 
