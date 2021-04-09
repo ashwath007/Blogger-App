374a7e501a0cc90e2a31aa22e13729fe210145b5
vicky <srivicky2000@gmail.com>
2021-04-09 15:14:18 +0530

    Read company somewat done

--- bApp/src/screen/ReadCompany.js
+++ bApp/src/screen/ReadCompany.js
@@ -1,5 +1,5 @@
 import React,{useEffect,useState} from 'react'
-import {View,Text,Image,SafeAreaView,ScrollView, TouchableOpacity} from 'react-native'
+import {View,Text,Image,SafeAreaView,ScrollView,Linking, TouchableOpacity} from 'react-native'
 import database from '@react-native-firebase/database';
 // import {getPostById} from '../action/post'
 import EmptyContainer from '../components/EmptyContainer'
@@ -7,8 +7,8 @@ import {Right,Left,H3,  Fab,Icon,Container} from 'native-base'
 import Tts from 'react-native-tts';
 import { Button } from 'react-native-paper';
 import { material,sanFranciscoSpacing,robotoWeights} from 'react-native-typography'
-
-
+import { SocialIcon } from 'react-social-icons';
+import { FacebookSocialButton } from "react-native-social-buttons";
 const ReadCompany = ({route}) => {
     const [loading,setLoading] = useState('');
 
@@ -63,7 +63,7 @@ const ReadCompany = ({route}) => {
                 setDescription(snapshot.val().description)
                 setCeo(snapshot.val().ceo)
                 setCname(snapshot.val().cname)
-                setCteam(snapshot.val().cteam)
+                setCteam(snapshot.val().coreteam)
                 setCtype(snapshot.val().ctype)
 
                 setDate(snapshot.val().date)
@@ -105,19 +105,69 @@ const ReadCompany = ({route}) => {
                         {cname}
                     </Text>
                 </View>
+                <Text style={{fontWeight:'bold',alignSelf:'center'}}>
+                            {headquartes}
+                        </Text>
+                        <View>
+
+                        <View style={{alignSelf:'center',marginTop:12,flexDirection:'row'}}>
+                            <Button
+                               transparent
+                             onPress={() => {
+                                Linking.openURL(facebook);
+                                }}
+                                
+                            >
+                            <Icon feather name="logo-facebook" style={{color: "black"}} onPress/>
+
+                            </Button>
+                            <Button
+                               transparent
+                             onPress={() => {
+                                Linking.openURL(linkedin);
+                                }}
+                                
+                            >
+                            <Icon feather name="logo-linkedin" style={{color: "black"}}/>
+                          
+                            </Button>
+
+                        </View>
+                        <View style={{alignSelf:'center',marginTop:8}}>
+                            <Text>
+                            {phone}
+
+                            </Text>
+                        </View>
+                    </View>
                 <View style={{padding:20}}>
-                    <Text style={{alignSelf:'center',color:'#242B2E',textAlign:'center',fontWeight:'400',fontSize:18}}>
+                    <Text style={{alignSelf:'center',color:'#242B2E',textAlign:'center',fontWeight:'600',fontSize:18}}>
                         {description}
                     </Text>
                     <View style={{padding:23}}>
-                    <View style={{backgroundColor:'#E1E1E1',height:40,alignItems:'center',justifyContent:'center'}}>
+                    <View style={{backgroundColor:'#E1E1E1',borderRadius:12,height:40,alignItems:'center',justifyContent:'center'}}>
                         <Text style={{fontSize:18}}>
                             {ctype}
                         </Text>
                     </View>
+                    
                     </View>
                    
                 </View>
+                <View style={{backgroundColor:'#FF6263'}}>
+                    
+                            <View>
+                                    <Text style={{padding:20,fontSize:18,color:'white'}}>
+                                            {story}
+                                        </Text>    
+                            </View>
+                    </View>
+                   
+                    <View style={{backgroundColor:'#000',padding:20,alignItems:'center'}}>
+                        <Text style={{color:"#fff",fontSize:15}}>
+                            {cteam}
+                        </Text>
+                    </View>
                 
             </View>
         </ScrollView>
